b25fabf56d2efe135ea8a6b0e63105978f6c1ba1
salwinfr <salwinfrancis29@gmail.com>
2024-11-13 14:42:56 +0530

    Update exp2_linear_using_circular.py

--- exp2_linear_using_circular.py
+++ exp2_linear_using_circular.py
@@ -1,19 +1,30 @@
-from numpy import *
-from matplotlib.pyplot import *
-def find_cir_conv(x1,x2):
-    N = len(x1)
-   
-    conv=zeros((N,N)) 
-    for i in range(N):
-        conv[:,i]=roll(x1,i)
-    result = dot(conv,x2)
-    return result  
-x1 = eval(input("Enter the input sequence   : "))
-x2 = eval(input("Enter the impulse sequence : "))
-l1=len(x1)
-m1=len(x2)
-N=l1+m1-1
-x1=np.pad(x1,(0,N-l1))
-x2=np.pad(x2,(0,N-m1))
-y = find_cir_conv(x1,x2)
-print("y(n)=",y)
+#linear_using_circular
+import numpy as np
+
+def circular_convolution(x, h):  # Define circular_convolution
+    N = max(len(x), len(h))
+    x = x + [0] * (N - len(x))
+    h = h + [0] * (N - len(h))
+    y = [0] * N
+    for n in range(N):
+        for k in range(N):
+            y[n] += x[k] * h[(n - k) % N]
+    return y
+
+def linear_using_circular(x, h):
+    N = len(x) + len(h) - 1
+    for i in range(N - len(x)):
+        x.append(0)
+    for i in range(N - len(h)):
+        h.append(0)
+    y = circular_convolution(x, h)  # Now circular_convolution is defined
+    y1 = np.array(y).reshape(1, N)
+    return y1
+
+a = "yes"
+while a.lower() == "yes":
+    x = list(eval(input("Enter the elements in x[n]:")))
+    h = list(eval(input("Enter the elements in h[n]:")))
+    y = linear_using_circular(x, h)
+    print(y)
+    a = input("Do you want to continue?yes/no: ")
