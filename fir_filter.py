279fe0d3deb65b445c6e7554385d426eda77b33e
salwinfr <salwinfrancis29@gmail.com>
2024-11-13 14:45:36 +0530

    Update fir_filter.py

--- fir_filter.py
+++ fir_filter.py
@@ -1,45 +1,97 @@
-from numpy import *
-from scipy.signal import freqz
-from matplotlib.pyplot import *
-from scipy.signal.windows import hamming,kaiser
-M = 100
-n = 100 
-sizen = arange(0, n)
-wn = zeros(n)
-wn[:M-1] = 1 
-wc = 0.3 * pi
-wn_hamming =hamming(M)
-hdn1 = zeros(n)
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sp
 
-def hd1(wc, n):
-    for i in range(0, n-1):
-        if i == 0:
-            hdn1[i] = wc/pi
-        else:
-            hdn1[i] = sin(wc * i) / (pi * i)
-    return hdn1
+# Define filter parameters
+N = 200
+omega_c = 0.15 * np.pi  # Convert to radians
+M = 50
+n = np.arange(-N, N)
 
-hdn = hd1(wc, n)
-hn = hdn * wn_hamming
+# Ideal filter (sinc function)
+h_ideal = np.sinc(omega_c / np.pi * n)  # normalized sinc function
 
-# Compute the frequency response
-w, h = freqz(hn)
-h=20*log10((h))
-# # Plot the impulse response
+# Plot ideal filter impulse response
+plt.figure(1)
+plt.plot(n, h_ideal)
+plt.title("Ideal Impulse Response")
+plt.xlabel("n")
+plt.ylabel("h_ideal[n]")
 
-plot(sizen, wn_hamming)
-title('Hamming Response')
-xlabel('Sample')
-ylabel('Amplitude')
-grid()
+# Rectangular window
+rect_window = np.array([1 if np.abs(val) < M else 0 for val in n])
+h_windowed_rect = h_ideal * rect_window
 
-# Plot the magnitude response
-figure()
-plot(w / pi,h)
-title('Frequency Response')
-xlabel('Normalized Frequency (×π rad/sample)')
-ylabel('Magnitude')
-grid()
-show()
+plt.figure(2)
+plt.plot(n, rect_window)
+plt.title("Rectangular Window")
+plt.xlabel("n")
+plt.ylabel("w[n]")
 
-print(hn)
+plt.figure(3)
+plt.plot(n, h_windowed_rect)
+plt.title("Windowed Impulse Response (Rectangular Window)")
+plt.xlabel("n")
+plt.ylabel("h[n]")
+
+# Hamming window
+hamm_window = np.hamming(2 * N)
+h_windowed_hamm = h_ideal * hamm_window
+
+plt.figure(4)
+plt.plot(n, hamm_window)
+plt.title("Hamming Window")
+plt.xlabel("n")
+plt.ylabel("w[n]")
+
+plt.figure(5)
+plt.plot(n, h_windowed_hamm)
+plt.title("Windowed Impulse Response (Hamming Window)")
+plt.xlabel("n")
+plt.ylabel("h[n]")
+
+# Hanning window
+hann_window = np.hanning(2 * N)
+h_windowed_hann = h_ideal * hann_window
+
+plt.figure(6)
+plt.plot(n, hann_window)
+plt.title("Hanning Window")
+plt.xlabel("n")
+plt.ylabel("w[n]")
+
+plt.figure(7)
+plt.plot(n, h_windowed_hann)
+plt.title("Windowed Impulse Response (Hanning Window)")
+plt.xlabel("n")
+plt.ylabel("h[n]")
+
+# Frequency response
+fig, axs = plt.subplots(3, 1, figsize=(10, 15))
+
+# Rectangular window frequency response
+w, H_rect = sp.freqz(h_windowed_rect)
+axs[0].plot(w / np.pi, 20 * np.log10(np.abs(H_rect)))
+axs[0].set_title("Frequency Response (Rectangular Window)")
+axs[0].set_xlabel("Normalized Frequency (×π rad/sample)")
+axs[0].set_ylabel("Magnitude (dB)")
+axs[0].grid()
+
+# Hamming window frequency response
+w, H_hamm = sp.freqz(h_windowed_hamm)
+axs[1].plot(w / np.pi, 20 * np.log10(np.abs(H_hamm)))
+axs[1].set_title("Frequency Response (Hamming Window)")
+axs[1].set_xlabel("Normalized Frequency (×π rad/sample)")
+axs[1].set_ylabel("Magnitude (dB)")
+axs[1].grid()
+
+# Hanning window frequency response
+w, H_hann = sp.freqz(h_windowed_hann)
+axs[2].plot(w / np.pi, 20 * np.log10(np.abs(H_hann)))
+axs[2].set_title("Frequency Response (Hanning Window)")
+axs[2].set_xlabel("Normalized Frequency (×π rad/sample)")
+axs[2].set_ylabel("Magnitude (dB)")
+axs[2].grid()
+
+plt.tight_layout()
+plt.show()
